cbac048b3ab1111d5e9d18c892da8e8f9747e479
Ryan Tin <ryantin526@gmail.com>
2023-03-24 00:43:41 +0100

    Remove comments and codestyle

--- after/plugin/nvim_tree.lua
+++ after/plugin/nvim_tree.lua
@@ -1,5 +1,3 @@
--- examples for your init.lua
-
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -59,4 +57,5 @@ require("nvim-tree").setup({
     },
     filters = {
         dotfiles = false,
-    }, })
+    },
+})
