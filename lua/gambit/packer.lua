7d4e629f9e045ea1dba076153bb941c57f9f1ddb
Ryan Tin <ryantin526@gmail.com>
2023-03-23 13:39:17 +0100

    Add Which-Key plugin w/ default settings

--- lua/gambit/packer.lua
+++ lua/gambit/packer.lua
@@ -99,11 +99,11 @@ return require('packer').startup(function(use)
         config = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
-            require("which-key").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
+            require("which-key").setup()
+            -- require("which-key").setup {
+            --     -- your configuration comes here
+            --     -- or leave it empty to use the default settings
+            -- }
         end
     }
 end)
