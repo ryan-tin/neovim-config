1d1a2e64e766cafbc40a401c5ed495dd5e96302f
Ryan Tin <ryantin526@gmail.com>
2023-03-24 00:42:16 +0100

    Change lsp keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -72,13 +72,13 @@ lsp.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts) -- search for a symbol
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "<leader>ls", vim.lsp.buf.workspace_symbol, opts) -- search for a symbol
+  vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts) -- code actions are "quick fixes", whenver you see an error message with (fix available), use this command..
+  vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, opts) -- quickly rename a symbol
   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts) -- code actions are "quick fixes", whenver you see an error message with (fix available), use this command..
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts) -- quickly rename a symbol
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts) -- help with function signature, in insert mode.
 end)
 
