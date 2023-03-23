08270375b6d82cbbe0a1a473f02f7a22752238c4
Ryan Tin <ryantin526@gmail.com>
2023-03-24 00:36:39 +0100

    Change harpooon file keymap to <leader>i

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>i", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 -- primeagen's keymaps work well for dvorak, not qewrty
