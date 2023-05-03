dba0bdbb7f7219921c10e317773f35f4fb6ee286
Ryan Tin <ryantin526@gmail.com>
2023-05-03 18:09:25 +0200

    quickfix & loclist nav

--- lua/gambit/remap.lua
+++ lua/gambit/remap.lua
@@ -62,8 +62,9 @@ vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- quick fix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-N>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
+-- loclist navigation
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
