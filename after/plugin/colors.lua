eeff1888c07688f105480159886a19a67aab0bb4
Ryan Tin <ryantin526@gmail.com>
2023-05-06 19:42:15 +0200

    Fix transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,11 @@
-function ColorMyPencils(color)
-	color = color or "everforest"
-	vim.cmd.colorscheme(color)
+-- function ColorMyPencils(color)
+function ColorMyPencils()
+    vim.cmd( "set background=dark" )
+    vim.cmd( "let g:everforest_background = 'hard'" )
+    vim.cmd( "let g:everforest_better_performance = 1" )
+    vim.cmd( "let g:everforest_transparent_background = 1" ) -- 2 conflicts with lualine
+	-- color = color or "everforest"
+	vim.cmd.colorscheme("everforest")
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
