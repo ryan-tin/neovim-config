edacab61d243538cef30fa2819a32f02eeacdc7a
Ryan Tin <ryantin526@gmail.com>
2023-05-07 14:57:19 +0200

    Add snipped for cpp code block in markdown

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,7 +1,21 @@
--- local status_ok, luasnip = pcall(require, "luasnip")
--- if not status_ok then
---   return
--- end
+local status_ok, luasnip = pcall(require, "luasnip")
+if not status_ok then
+  return
+end
 
 vim.keymap.set({"i", "s" }, "<Tab>", "<cmd>lua require'luasnip'.jump(1)<CR>")
 vim.keymap.set({"i", "s" }, "<S-Tab>", "<cmd>lua require'luasnip'.jump(-1)<CR>")
+
+local s = luasnip.snippet
+local t = luasnip.text_node
+local i = luasnip.insert_node
+
+luasnip.add_snippets("markdown", {
+    -- custom cpp code snippet for markdown
+    s("cpp", {
+        t({ "```cpp", "" }), i(1, "code"), t({ "", "```" })
+    })
+})
+
+-- read more about how to create custom snippets here
+-- https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md
