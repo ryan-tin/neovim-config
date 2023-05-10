366173ecce728133681394390d77a602ac062976
Ryan Tin <ryantin526@gmail.com>
2023-05-10 03:05:07 +0200

    colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,7 @@
 -- function ColorMyPencils(color)
 function Color()
     vim.cmd( "set background=dark" )
-    vim.cmd( "let g:everforest_background = 'hard'" )
+    vim.cmd( "let g:everforest_background = 'soft'" )
     vim.cmd( "let g:everforest_better_performance = 1" )
     vim.cmd( "let g:everforest_transparent_background = 1" ) -- 2 conflicts with lualine
 	vim.cmd.colorscheme("everforest")
